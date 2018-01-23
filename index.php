f5715bbd7da00824f1d820bca7611e00fd0cce29
OmarElKhatib <35725554+OmarElKhatibCS@users.noreply.github.com>
2018-01-23 13:43:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="ar">
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="images/miniLogo.png" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Omar ElKhatib Developer</title>
+
+    <!-- Bootstrap -->
+      <link href="css/reset.css" rel="stylesheet" type="text/css"> 
+      <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">  
+      <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>      
+      <span name="top"></span>
+      <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img style="width:50px" src="images/logo.png" class="img-responsive"/></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">HOME <span class="sr-only">(current)</span></a></li>
+        <li><a href="#exBox">Experience</a></li>
+          <li><a href="#ProjectsTitle">Projects</a></li>
+          <li><a href="#aboutTitle">About</a></li>
+          <li><a href="#ctTitle">contactUs</a></li>
+      </ul>
+        
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+      
+      <div id="ImgWo" class="col-lg-12">
+          <h1>Omar Developer</h1>
+          <p>“Everybody in this country should learn to program a computer, because it teaches you how to think”
+— Steve Jobs</p>
+      </div>
+      
+      <!-- Begin ExperienceBox -->
+      
+      <div id="exBox" class="col-lg-12">
+        <p>Experiences</p>
+      </div>
+      
+      <div id="exs" class="col-lg-12 experience">
+          
+          <!-- begin HtmlBox -->
+        <div class="col-lg-6 html">
+            <p>HTML<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+            
+          </div> <!-- end html -->
+          
+          <!-- Css beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>CSS<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+    <span class="sr-only">90% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end cssBox -->
+          
+          
+          <!-- AS3 beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>AS3<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end AS3 -->
+          
+            <!-- Unity3D beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>Unity3D<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end Unity3D -->
+          
+          
+           <!-- c# beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>c#<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- c# endBox -->
+          
+          <!-- java beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>java<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end java -->
+          
+          <!-- JS beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>JS<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end JS -->
+          
+          <!-- Python beginBox -->
+          <div class="col-lg-6 html">
+            
+              <p>Python<p>
+            <div class="progress">
+  <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+    <span class="sr-only">100% Complete (success)</span>
+  </div>
+</div>
+              
+          </div> <!-- end Python -->
+          
+          
+      </div> <!-- end ExperienceBox -->
+      
+      
+      <!-- begin Projects-->
+      
+      <div class="col-lg-12 Projects">
+            <span id="ProjectsTitle"><p>Projects</p></span>
+          
+          <div class="col-lg-12">
+              
+          <!-- begin Project1-->
+          <div id="proj" class="col-lg-6"> 
+            <div class="col-lg-6">
+              <img src="images/saveH.jpg" class="img-responsive"/>
+              </div>
+                <div class="col-lg-6">
+                <h1>saveH</h1>
+                    <p>it's beta game build with unity3d Engine , you need to help your girl friend</p>
+                    <a target="_blank" href="https://userscloud.com/banarcm9j5zg"><button>Download</button></a>
+              </div>
+          </div><!-- end Project1 -->
+          
+           <!-- begin Project2-->
+          <div id="proj" class="col-lg-6"> 
+            <div class="col-lg-6">
+              <img src="images/AAcopy.jpg" class="img-responsive"/>
+              </div>
+                <div class="col-lg-6">
+                <h1>AA Copy</h1>
+                    <p>it's beta game build with unity3d Engine , look like AA game on Play Store</p>
+                    <a target="_blank" href="https://userscloud.com/0oo5ja1aeey4"><button>Download</button></a>
+              </div>
+          </div><!-- end Project2 -->
+              </div>
+          
+          <div class="col-lg-12">
+          
+          <!-- begin Project3-->
+          <div id="proj" class="col-lg-6"> 
+            <div class="col-lg-6">
+              <img src="images/okProJump.jpg" class="img-responsive"/>
+              </div>
+                <div class="col-lg-6">
+                <h1>OkProJump</h1>
+                    <p>it's beta game build with unity3d Engine , You Jump From Rock to another</p>
+                    <a target="_blank" href="https://userscloud.com/gp33va40s9mg"><button>Download</button></a>
+              </div>
+          </div><!-- end Project4 -->
+          
+          <!-- begin Project4-->
+          <div id="proj" class="col-lg-6"> 
+            <div class="col-lg-6">
+              <img src="images/ChildApp.jpg" class="img-responsive"/>
+              </div>
+                <div class="col-lg-6">
+                <h1>Quiz Game</h1>
+                    <p>it's beta game build with Flash (AS3) , You Need to Pick To word correspond for the Image</p>
+                    <a target="_blank" href="https://userscloud.com/olxqb10nl2wo"><button>Download</button></a>
+              </div>
+          </div><!-- end Project4 -->
+              </div>
+          
+      </div>
+      <!-- end Projects -->
+      
+      <!-- about Begin-->
+      <div class="col-lg-12 about">
+         <span id="aboutTitle"><h1>ABOUT</h1></span>
+
+          <div class="col-lg-12">
+              <span id="AboutSubTitle"></span>
+              <p>
+              I am a developer who has gathered a big experience in programing and web designing through <b>self-study</b>. At present, I am a student at school. However, I invest my free and summer time in developing my programming skills. I am passionate about this field of knowledge that fosters logical thinking and creativity. I am working hard to transform this passion into professionality; thus, I have decided to major in computer science.<br/>  
+As many computer and tech amateurs, my journey into game programming had started with a child’s passion towards games. Excited to explore the world of game development, I started to dive into the world of computing. Having access to Internet has enabled me to engage in topics writing in an electronic forum. Exposed to fellows’ writings, I became aware of a big industry in computing which is “Web Designing”. I have designed many Webs with the latest Bootstrap framework. My curiosity has led me into programming. Throughout my exploration and learning experiences, I have learned many programming languages such as HTML, CSS, HTML5, CSS3, JAVA, C# in Unity3d, and AS3 in Flash. Currently, I am learning Python. Besides, I am interested in Photoshop and Blender3D. My ambition carries me to dream of the position of a software engineer. My dreams doesn’t stop here; I really want to help other tech amateurs exploring their passion towards programming through free training.
+
+              </p>
+          </div>
+          
+      </div>
+      <!-- about end -->
+      
+      <!-- Contact Begin-->
+      <div class="col-lg-12 contactus">
+          
+         <span id="ctTitle"><h1>ContactUs</h1></span>
+          
+          <div class="col-lg-12">
+            <p> <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> : 00961 70 661 903</p>
+            <p>Email : omarelkhatib.developer@mail.com</p>
+            <p>Facebook account : <a target="_blank" href="https://www.facebook.com/omarelkhatibdeveloper">Omar ElKhatib</a></p>  
+        </div>
+          
+      </div>
+      <!-- Contact end -->
+      
+      <!-- footer Begin-->
+      <div class="col-lg-12 footerEnd">
+          
+         <footer> <a href="#top"> <span class="glyphicon glyphicon-arrow-up"></span> Coded And Designed By OmarElKhatib <span class="glyphicon glyphicon-arrow-up"></span> </a>  </footer>
+          
+      </div>
+      <!-- footer end -->
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+      
+      <script>
+      $(document).ready(function() {
+   
+    $('a[href=#top]').click(function(){
+        $('html, body').animate({scrollTop:0}, 'slow');
+        return false;
+    });
+
+});
+      </script>
+      
+  </body>
+</html>
